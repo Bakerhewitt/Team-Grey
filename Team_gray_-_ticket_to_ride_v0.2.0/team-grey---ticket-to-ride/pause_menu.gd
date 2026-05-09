d83e89a1653f9d0744835d9dfa6eed40a194db9f
EggRollington <mjw1001@gmail.com>
2026-05-09 18:30:19 -0400

    updated some code

--- Team_gray_-_ticket_to_ride_v0.2.0/team-grey---ticket-to-ride/pause_menu.gd
+++ Team_gray_-_ticket_to_ride_v0.2.0/team-grey---ticket-to-ride/pause_menu.gd
@@ -1,16 +1,16 @@
 extends CanvasLayer
 
 func _ready():
-	# This ensures the menu keeps working even when the game is paused
+	
 	process_mode = Node.PROCESS_MODE_ALWAYS 
 	get_tree().paused = true
 
 func _on_resume_button_pressed():
 	get_tree().paused = false
-	queue_free() # Closes the menu
+	queue_free() 
 
 func _on_save_button_pressed():
-	# Generate a name based on current time
+	
 	var time = Time.get_datetime_dict_from_system()
 	var save_name = "Save_%d-%02d-%02d_%02d%02d.dat" % [
 		time.year, time.month, time.day, time.hour, time.minute
@@ -21,7 +21,7 @@ func _on_save_button_pressed():
 		main_ui.save_hotseat_game(save_name)
 		print("Saved as: ", save_name)
 	
-	# Optional: Show a "Saved!" message or just close the menu
+	
 	_on_resume_button_pressed()
 
 func _on_quit_button_pressed():
